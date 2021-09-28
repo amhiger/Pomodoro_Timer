107b375e8ff4f67537f896d9d90793d743577836
Adam Higer <86610022+amhiger@users.noreply.github.com>
2021-09-28 11:59:07 -0400

    First Commit

---
+++ src/pomodoro/Buttons.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { minutesToDuration, /* secondsToDuration */ } from "../utils/duration";
+
+function Buttons({ focusDuration, handleFocusMinus, handleBreakMinus, handleBreakPlus, session, handleFocusPlus, breakDuration }) {
+    return (
+        <div className="row">
+        <div className="col">
+          <div className="input-group input-group-lg mb-2">
+            <span className="input-group-text" data-testid="duration-focus">
+              {/* TODO: Update this text to display the current focus session duration */}
+              Focus Duration: {minutesToDuration(focusDuration)}
+            </span>
+            <div className="input-group-append">
+              {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-testid="decrease-focus"
+                onClick={handleFocusMinus}
+                disable={session || focusDuration === 5}
+              >
+                <span className="oi oi-minus" />
+              </button>
+              {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-testid="increase-focus"
+                onClick={handleFocusPlus}
+                disable={session || focusDuration === 60}
+              >
+                <span className="oi oi-plus" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="float-right">
+            <div className="input-group input-group-lg mb-2">
+              <span className="input-group-text" data-testid="duration-break">
+                {/* TODO: Update this text to display the current break session duration */}
+                Break Duration: {minutesToDuration(breakDuration)}
+              </span>
+              <div className="input-group-append">
+                {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-testid="decrease-break"
+                  onClick={handleBreakMinus}
+                  disable={session || breakDuration === 1}
+
+                >
+                  <span className="oi oi-minus" />
+                </button>
+                {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-testid="increase-break"
+                  onClick={handleBreakPlus}
+                  disable={session || breakDuration === 15}
+                >
+                  <span className="oi oi-plus" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default Buttons;
